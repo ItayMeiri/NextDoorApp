c24d2f2e4b02184405e7d810605e8a1233e9c4b4
May-Cohen <74496472+May-Cohen@users.noreply.github.com>
2021-12-15 14:50:36 +0200

    login update

--- app/src/main/java/com/app/nextdoor/LoginActivity.java
+++ app/src/main/java/com/app/nextdoor/LoginActivity.java
@@ -1,10 +1,11 @@
 package com.app.nextdoor;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,12 +16,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+        import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     EditText Email, Password;
     Button SighIn;
-    TextView SighUP;
+    TextView SighUp;
     FirebaseAuth myAuth;
 
     @Override
@@ -28,46 +29,54 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Email = findViewById(R.id.Email1);
+        Password = findViewById(R.id.Password1);
+        SighIn = findViewById(R.id.button1);
+        SighUp = findViewById(R.id.textView1);
         myAuth = FirebaseAuth.getInstance();
-        Email = findViewById(R.id.Email);
-        Password = findViewById(R.id.Password);
-        SighIn = findViewById(R.id.button);
-        SighUP = findViewById(R.id.textView);
-        SighIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = Email.getText().toString();
-                String password = Password.getText().toString();
-                if (email.isEmpty()) {
-                    Email.setError("Please enter your Email address");
-                    Email.requestFocus();
-                } else if (password.isEmpty()) {
-                    Password.setError("Please enter your password");
-                    Password.requestFocus();
-                } else if (email.isEmpty() && password.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Email and Password are empty", Toast.LENGTH_SHORT).show();
-                } else if (!(email.isEmpty() && password.isEmpty())) {
-                    myAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (!task.isSuccessful()) {
-                                Toast.makeText(LoginActivity.this, "Login failed,please try again", Toast.LENGTH_SHORT).show();
-                            } else {
-                                startActivity(new Intent(LoginActivity.this, HomePageActivity.class));
-                            }
-                        }
-                    });
-                } else {
-                    Toast.makeText(LoginActivity.this, "An error occurred.Please try again", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
 
         SighIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this, LoginActivity.class);
+                Login();
+            }
+        });
+
+        SighUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, RegistrationActivity.class);
                 startActivity(i);
+            }
+        });
+    }
+
+    private void Validation(String email, String password){
+        if (TextUtils.isEmpty(email)){
+            Email.setError("Please enter your Email address");
+        }
+        else if (TextUtils.isEmpty(password)){
+            Password.setError("Please enter your password");
+        }
+        else if (!(TextUtils.isEmpty(email)) && !(Patterns.EMAIL_ADDRESS.matcher(email).matches())){
+            Email.setError("Please enter valid Email address");
+        }
+    }
+
+    private void Login(){
+        String email = Email.getText().toString();
+        String password = Password.getText().toString();
+        Validation(email,password);
+        myAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent i = new Intent(LoginActivity.this,HomePageActivity.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,"Login failed",Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
