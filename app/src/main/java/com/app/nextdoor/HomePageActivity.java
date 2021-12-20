f87dc9dd9c14d91640770664edad20a0a9cec6dc
May-Cohen <74496472+May-Cohen@users.noreply.github.com>
2021-12-20 17:04:43 +0200

    HomePage update

--- app/src/main/java/com/app/nextdoor/HomePageActivity.java
+++ app/src/main/java/com/app/nextdoor/HomePageActivity.java
@@ -2,17 +2,27 @@ package com.app.nextdoor;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,11 +32,13 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.zip.Inflater;
 
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     Button search;
+    Button menu;
     Spinner sp;
     ArrayAdapter<String> adapter;
 
@@ -35,7 +47,9 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        search = findViewById(R.id.button3);
+        search = findViewById(R.id.search);
+        menu = findViewById(R.id.button3);
+
         sp = findViewById(R.id.spinner);
         String [] items = new String[]{"Language","English","French","Hebrew"};
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,items){
@@ -74,7 +88,33 @@ public class HomePageActivity extends AppCompatActivity {
             }
         });
 
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(HomePageActivity.this,view);
+                popup.setOnMenuItemClickListener(HomePageActivity.this);
+                popup.inflate(R.menu.navigation_menu);
+                popup.show();
+            }
+        });
+    }
 
-
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.myprofile:
+                Intent i = new Intent(HomePageActivity.this,ProfileActivity.class);
+                startActivity(i);
+                break;
+            case R.id.settings:
+                Intent j = new Intent(HomePageActivity.this,SettingsActivity.class);
+                startActivity(j);
+                break;
+            case R.id.logOut:
+                Intent k = new Intent(HomePageActivity.this,LoginActivity.class);
+                startActivity(k);
+                break;
+        }
+        return false;
     }
 }
